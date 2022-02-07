0d9e85a28cdf985a463df9fffbe45b058861a0a2
applefung <804111435@qq.com>
2022-02-07 23:56:14 +0800

    add cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AllExceptionFilter } from './filters/all-exception.filter';
 import { ValidationException } from './utils/exception';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalFilters(new AllExceptionFilter());
   app.useGlobalPipes(
     new ValidationPipe({
